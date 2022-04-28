65c1a78f2a5e7e88f740775c366215f61728dc87
Ethan Wright <wrightet707@gmail.com>
2022-04-27 21:47:54 -0700

    write get routes

--- test_api/api.py
+++ test_api/api.py
@@ -1,21 +1,29 @@
-from flask import flask
+from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
+
+users_path = "./users.csv"
+locations_path = "./locations.csv"
 
 app = Flask(__name__)
 api = Api(app)
 
 class Users(Resource):
-    pass
-
+    def get(self):
+        data = pd.read_csv(users_path)
+        data = data.to_dict()
+        return {'data': data},200
 
 
 class Locations(Resource):
-    pass
+    def get(self):
+        data = pd.read_csv(locations_path)
+        data = data.to_dict()
+        return {'data': data},200
 
 api.add_resource(Locations, "/locations")
 api.add_resource(Users, '/users')
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run()
