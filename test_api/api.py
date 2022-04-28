28450eb594a8710dcf4ee314a0feb2b38d2edb1a
Ethan Wright <wrightet707@gmail.com>
2022-04-27 22:35:15 -0700

    make a post to user

--- test_api/api.py
+++ test_api/api.py
@@ -14,7 +14,28 @@ class Users(Resource):
         data = pd.read_csv(users_path)
         data = data.to_dict()
         return {'data': data},200
-
+    
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('userId', required=True, type=int)
+        parser.add_argument('name', required=True, type=str)
+        parser.add_argument('city', required=True, type=str)
+        args = parser.parse_args()
+        data = pd.read_csv(users_path)
+        if args['userId'] in data['userId']:
+            return {
+                'message': f"{args['userId']} already exists"
+            }
+        else:
+            data = data.append(
+                {
+                    'userId' :args['userId'],
+                    'name' :args['name'],
+                    'city' :args['city'],
+                }, ignore_index=True
+            )
+            data.to_csv(users_path, index=False)
+            return {'data': data.to_dict()}, 200
 
 class Locations(Resource):
     def get(self):
@@ -23,7 +44,7 @@ class Locations(Resource):
         return {'data': data},200
 
 api.add_resource(Locations, "/locations")
-api.add_resource(Users, '/users')
+api.add_resource(Users, "/users")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
