676c8375343283fc55f83a5453962f06369d0501
Ethan Wright <wrightet707@gmail.com>
2022-04-27 21:20:46 -0700

    build out basic class and end point, get the csv files

--- test_api/api.py
+++ test_api/api.py
@@ -3,3 +3,19 @@ from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
 
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    pass
+
+
+
+class Locations(Resource):
+    pass
+
+api.add_resource(Locations, "/locations")
+api.add_resource(Users, '/users')
+
+if __name__ =="__main__":
+    app.run()
---
+++ test_api/requests.py
@@ -0,0 +1,2 @@
+import pip._vendor.requests
+res = requests.get(https://gist.githubusercontent.com/jamescalam/)
