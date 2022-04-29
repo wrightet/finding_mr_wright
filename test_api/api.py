4f13b1d2000bd8171214cfa9c8b9a9f09261f0b3
Ethan Wright <wrightet707@gmail.com>
2022-04-28 22:04:20 -0700

    put request

--- test_api/api.py
+++ test_api/api.py
@@ -36,7 +36,37 @@ class Users(Resource):
             )
             data.to_csv(users_path, index=False)
             return {'data': data.to_dict()}, 200
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
 
+        # read our CSV
+        data = pd.read_csv('users.csv')
+        
+        if args['userId'] in list(data['userId']):
+            # evaluate strings of lists to lists
+            data['locations'] = data['locations'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+            # select our user
+            user_data = data[data['userId'] == args['userId']]
+
+            # update user's locations
+            user_data['locations'] = user_data['locations'].values[0] \
+                .append(args['location'])
+            
+            # save back to CSV
+            data.to_csv('users.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            return {
+            'message': f"'{args['userId']}' user not found."
+            }, 404
+        
 class Locations(Resource):
     def get(self):
         data = pd.read_csv(locations_path)
